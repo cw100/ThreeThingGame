6e9d9dad903ef1688400b2310ee8bb6bbebcf807
connor <connordixie@gameviewer.co.uk>
2014-10-31 19:36:41 +0000

    First

--- RoboticRainbowCats/RoboticRainbowCats/Program.cs
+++ RoboticRainbowCats/RoboticRainbowCats/Program.cs
@@ -1,15 +1,26 @@
-﻿using System;
+﻿#region Using Statements
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+#endregion
 
 namespace RoboticRainbowCats
 {
-    class Program
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
+            using (var game = new Game1())
+                game.Run();
         }
     }
+#endif
 }
--- RoboticRainbowCats/RoboticRainbowCats/Properties/AssemblyInfo.cs
+++ RoboticRainbowCats/RoboticRainbowCats/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RoboticRainbowCats")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RoboticRainbowCats")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d53ca4b1-b0aa-4901-83ee-bc332508f3f7")]
+[assembly: Guid("fe80e558-a206-4247-8028-229a296fb745")]
 
 // Version information for an assembly consists of the following four values:
 //
